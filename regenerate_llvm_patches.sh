461454b846e470ea726a9714d1e7ed36e5f44c11
Simon Moll <simon.m.moll@googlemail.com>
2012-03-01 11:50:41 +0000

    added script for regenerating svn patch files from the local LLVM and clang copies

---
+++ regenerate_llvm_patches.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LLVM_REVISION=`svn info ../.. | grep Revision | sed 's/Revision: //'`
+LLVM_PATCH="patches/llvm_opencl_patch_r${LLVM_REVISION}.patch"
+CLANG_REVISION=`svn info ../../tools/clang | grep Revision | sed 's/Revision: //'`
+CLANG_PATCH="patches/clang_opencl_patch_r${CLANG_REVISION}.patch"
+
+mkdir -p patches
+
+echo -n "Regenerating Clang patch file (r${CLANG_REVISION}) .."
+svn diff ../../tools/clang > ${CLANG_PATCH}
+echo "DONE"
+
+echo -n "Regenerating LLVM patch file  (r${LLVM_REVISION}) .."
+svn diff ../.. > ${LLVM_PATCH}
+echo "DONE"
